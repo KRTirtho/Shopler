4f7d33c3be18415cbb488bf1e08f899d6b007f44
KR Tirtho <kbroy2016@gmail.com>
2020-06-26 22:47:39 +0600

    Fixed no CSS Issue

--- server.js
+++ server.js
@@ -44,9 +44,6 @@ app.use(passport.session())
 //For all routes in production
 if(process.env.NODE_ENV==="production"){
   app.use(express.static("client/build"))
-  app.get("*", (req, res)=>{
-    res.sendFile(path.resolve(__dirname , "client", "build", "index.html"))
-  })
 }
 
 app.use("/api", router);
